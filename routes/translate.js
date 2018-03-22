56b8c5d2d4588a486d7d063e616bc7a5c35d5131
DESKTOP-CKBSVRP\Lena <puchochek@gmail.com>
2018-03-22 22:16:45 +0300

    Add translation

--- routes/translate.js
+++ routes/translate.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET translate listing. */
 router.get('/', function (req, res, next) {
   let word = req.query.word;
-  let translatedWord = word.toUpperCase();
-  let result = {
-    word: translatedWord
-  };
-  res.send(JSON.stringify(result));
+  axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + process.env.KEY + '&text=' + word + '&lang=en-ru')
+    .then(function (response) {
+      console.log(response.data.text[0]);
+      let result = {
+        word: response.data.text[0]
+      };
+      res.send(JSON.stringify(result));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 module.exports = router;
