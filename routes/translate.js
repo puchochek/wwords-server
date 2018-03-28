da61dd2f1bd3d8933830131e963ef8eb4eddb8ee
DESKTOP-CKBSVRP\Lena <puchochek@gmail.com>
2018-03-28 22:32:27 +0300

    Change API endpoint (translate to dictionary)

--- routes/translate.js
+++ routes/translate.js
@@ -5,12 +5,22 @@ var axios = require('axios');
 /* GET translate listing. */
 router.get('/', function (req, res, next) {
   let word = req.query.word;
-  axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + process.env.KEY + '&text=' + word + '&lang=en-ru')
+  axios.get('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=' + process.env.KEY + '&lang=en-ru&text=' + word)
     .then(function (response) {
-      console.log(response.data.text[0]);
+      
+      console.log(response.data.def);
+
+      let words = [];
+      response.data.def.forEach(defPosition => {
+        defPosition.tr.forEach(tr => {
+          words.push(tr.text);
+        });
+      });
+
       let result = {
-        word: response.data.text[0]
+        words: words
       };
+
       res.send(JSON.stringify(result));
     })
     .catch(function (error) {
