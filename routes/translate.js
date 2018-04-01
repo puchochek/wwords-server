a40e49e6e6de4d7e085327d614c8362e0a044426
DESKTOP-CKBSVRP\Lena <puchochek@gmail.com>
2018-04-01 22:48:57 +0300

    Add .env folder and promise-redise module

--- routes/translate.js
+++ routes/translate.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var redis = require('promise-redis')();
+var client = redis.createClient(process.env.REDIS_URL);
 
 /* GET translate listing. */
 router.get('/', function (req, res, next) {
+  
+  client.set('mykey', 'myvalue')
+    .then(console.log)
+    .catch(console.log)
+
   let word = req.query.word;
   axios.get('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=' + process.env.KEY + '&lang=en-ru&text=' + word)
     .then(function (response) {
-      
+
       console.log(response.data.def);
 
       let words = [];
